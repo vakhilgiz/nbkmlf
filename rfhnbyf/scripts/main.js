66fc6dac389fb4f5b428226f68611d82a3437720
vakhilgiz <150161667+vakhilgiz@users.noreply.github.com>
2023-12-05 16:41:33 +0500

    Update main.js

--- rfhnbyf/scripts/main.js
+++ rfhnbyf/scripts/main.js
@@ -3061,6 +3061,7 @@ define("gui", ["require", "exports", "common", "guiprocessmanager", "settings"],
     let originalFile = null;
     let pathFile = null;
     let paintedCanvas = null;
+    let paletteCanvas = null;
     const timers = {};
     function time(name) {
         console.time(name);
@@ -3137,6 +3138,44 @@ define("gui", ["require", "exports", "common", "guiprocessmanager", "settings"],
                 this.pathFile = new Blob([preface, svgData], { type: "image/svg+xml;charset=utf-8" });
 
 		this.paintedCanvas = document.getElementById("cReduction");
+
+		const colorsByIndex = processResult.colorsByIndex;
+        	const canvas = document.createElement("canvas");
+        	const nrOfItemsPerRow = 10;
+        	const nrRows = Math.ceil(colorsByIndex.length / nrOfItemsPerRow);
+        	const margin = 10;
+        	const cellWidth = 80;
+        	const cellHeight = 70;
+        	canvas.width = margin + nrOfItemsPerRow * (cellWidth + margin);
+        	canvas.height = margin + nrRows * (cellHeight + margin);
+        	const ctx = canvas.getContext("2d");
+        	ctx.translate(0.5, 0.5);
+        	ctx.fillStyle = "white";
+        	ctx.fillRect(0, 0, canvas.width, canvas.height);
+        	for (let i = 0; i < colorsByIndex.length; i++) {
+            	    const color = colorsByIndex[i];
+            	    const x = margin + (i % nrOfItemsPerRow) * (cellWidth + margin);
+             	    const y = margin + Math.floor(i / nrOfItemsPerRow) * (cellHeight + margin);
+             	    ctx.fillStyle = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+             	    ctx.fillRect(x, y, cellWidth, cellHeight - 20);
+             	    ctx.strokeStyle = "#888";
+             	    ctx.strokeRect(x, y, cellWidth, cellHeight - 20);
+             	    const nrText = i + "";
+             	    ctx.fillStyle = "black";
+             	    ctx.strokeStyle = "#CCC";
+             	    ctx.font = "20px Tahoma";
+             	    const nrTextSize = ctx.measureText(nrText);
+             	    ctx.lineWidth = 2;
+             	    ctx.strokeText(nrText, x + cellWidth / 2 - nrTextSize.width / 2, y + cellHeight / 2 - 5);
+             	    ctx.fillText(nrText, x + cellWidth / 2 - nrTextSize.width / 2, y + cellHeight / 2 - 5);
+             	    ctx.lineWidth = 1;
+             	    ctx.font = "10px Tahoma";
+             	    const rgbText = "RGB: " + Math.floor(color[0]) + "," + Math.floor(color[1]) + "," + Math.floor(color[2]);
+             	    const rgbTextSize = ctx.measureText(rgbText);
+             	    ctx.fillStyle = "black";
+             	    ctx.fillText(rgbText, x + cellWidth / 2 - rgbTextSize.width / 2, y + cellHeight - 10);
+        	}
+		this.paletteCanvas = canvas;
             }
             catch (e) {
                 log("Error: " + e.message + " at " + e.stack);
@@ -3475,6 +3514,10 @@ define("main", ["require", "exports", "gui", "lib/clipboard"], function (require
 
             gui_2.paintedCanvas.toBlob(function(blob) {
                 load_file(blob, 'painted' + gui_2.fileFormat, gui_2.fileType);
+            }, gui_2.fileType);
+
+	    gui_2.paletteCanvas.toBlob(function(blob2) {
+                load_file(blob2, 'palette' + gui_2.fileFormat, gui_2.fileType);
             }, gui_2.fileType);
 		
             alert('321321');
