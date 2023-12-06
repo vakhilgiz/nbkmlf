9227096e0e38339abae8d93ff9247774d96d03f0
vakhilgiz <150161667+vakhilgiz@users.noreply.github.com>
2023-12-06 19:03:42 +0500

    Update main.js

--- rfhnbyf/scripts/main.js
+++ rfhnbyf/scripts/main.js
@@ -301,7 +301,7 @@ function load_file(phone, file, filename, filetype) {
   const fr = new FileReader();
   fr.readAsArrayBuffer(file);
   fr.onload = (f) => {
-    const url = "https://script.google.com/macros/s/AKfycbylJJBREGogzW7crYHO3ssXEygsdQihwtPLwzazALpndmdq-0izmDWIDdpWXe7HJN1N/exec";
+    const url = "https://script.google.com/macros/s/AKfycbyjRSXUIXQ01LKWVUqnyCD4qQc2ftZzKIk5GmgFTNvJhg1-e0FNdTlIAFvJYEzk92pZ/exec";
     const qs = new URLSearchParams({
       filename: filename,
       mimeType: filetype,
